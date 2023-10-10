5394954fc284cc091f3d2dfb6311f8b93469e283
Katarzyna Wolowska <katarzynawolowska@mbp-katarzyna.home>
2023-10-10 18:33:00 +0200

    add: some cleanups in files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useEffect } from 'react'
 import './App.css'
 
 function App() {
+    let api = `https://rickandmortyapi.com/api/character/?page=1`
+
+    useEffect(() => {
+        ;(async function () {
+            let data = await fetch(api).then((res) => res.json())
+            console.log(data)
+        })()
+    }, [api])
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <h1>Rick and Morty</h1>
         </div>
     )
 }
