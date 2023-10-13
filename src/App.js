7657ac74ace923584bdf03731d90e9ef0010f608
Katarzyna Wolowska <katarzynawolowska@mbp-katarzyna.home>
2023-10-13 12:50:48 +0200

    add: pagination

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react'
-import { Typography, Container } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import { Typography, Container, Pagination, Box, CircularProgress } from '@mui/material'
 import CharacterList from './components/CharacterList/CharacterList'
 
 function App() {
+    const [fetchedData, updateFetchedData] = useState([])
+    const { info, results } = fetchedData
+
+    const [page, setPage] = useState(1)
+
+    const api = `https://rickandmortyapi.com/api/character/?page=${page}`
+
+    useEffect(() => {
+        fetch(api)
+            .then((response) => response.json())
+            .then((data) => {
+                updateFetchedData([])
+                setTimeout(() => {
+                    updateFetchedData(data)
+                }, 500)
+            })
+            .catch((error) => {
+                console.error('Error fetching character data:', error)
+            })
+    }, [api])
+
     return (
         <div className="App">
             <Container sx={{ p: 3 }}>
@@ -10,7 +31,31 @@ function App() {
                     Rick and Morty
                 </Typography>
 
-                <CharacterList />
+                {results?.length > 0 ? (
+                    <>
+                        <CharacterList characters={results} />
+
+                        <Pagination
+                            page={page}
+                            count={info?.pages}
+                            size="large"
+                            onChange={(e) => {
+                                setPage(Number(e.target.textContent))
+                            }}
+                            sx={{ display: 'flex', justifyContent: 'center' }}
+                        />
+                    </>
+                ) : (
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            my: 10,
+                        }}
+                    >
+                        <CircularProgress size={50} />
+                    </Box>
+                )}
             </Container>
         </div>
     )
--- src/components/CharacterList/CharacterList.js
+++ src/components/CharacterList/CharacterList.js
@@ -1,25 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import CharacterCard from '../CharacterCard/CharacterCard'
 import { Grid } from '@mui/material'
 
-const CharacterList = () => {
-    const [characters, setCharacters] = useState([])
-
-    useEffect(() => {
-        fetch('https://rickandmortyapi.com/api/character')
-            .then((response) => response.json())
-            .then((data) => {
-                setCharacters(data.results)
-            })
-            .catch((error) => {
-                console.error('Error fetching character data:', error)
-            })
-    }, [])
-
+const CharacterList = ({ characters }) => {
     return (
         <Grid container justifyContent="center" gap={4} my={4}>
             {characters?.map((character) => (
-                <Grid item>
+                <Grid item key={character.id}>
                     <CharacterCard key={character.id} character={character} />
                 </Grid>
             ))}
