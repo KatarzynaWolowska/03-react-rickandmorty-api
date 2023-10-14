77f8e561fa40625eba5e2109dc1f115cac2efd11
Katarzyna Wolowska <katarzynawolowska@mbp-katarzyna.home>
2023-10-14 02:04:57 +0200

    add: hover effect on card

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import { Box, Card, CardContent, Typography } from '@mui/material'
 import StatusChip from '../StatusChip/StatusChip'
+import styles from './CharacterCard.module.css'
 
 const CharacterCard = ({ character }) => {
     const { id, name, image, species, status } = character
@@ -8,8 +9,10 @@ const CharacterCard = ({ character }) => {
     return (
         <Link to={`/character/${id}`} style={{ textDecoration: 'none' }}>
             <Card sx={{ maxWidth: '320px' }}>
-                <CardContent sx={{ position: 'relative' }}>
-                    <Box component="img" src={image} alt={name} maxWidth="100%"></Box>
+                <CardContent className={styles.grow} sx={{ position: 'relative' }}>
+                    <Box sx={{ width: '100%', height: '288px', overflow: 'hidden', mb: 2 }}>
+                        <Box component="img" src={image} alt={name} maxWidth="100%"></Box>
+                    </Box>
                     <Typography sx={{ fontSize: '1.2rem', fontWeight: '700' }}>{name}</Typography>
                     <Typography sx={{ fontSize: '1rem' }}>{species}</Typography>
                     <StatusChip status={status} />
